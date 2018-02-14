62c49e99c5d6b93a5ccb38cf24bf207b315403e7
pedrobrito17@gmail.com <pedrobrito17@gmail.com>
2018-02-14 20:58:19 -0300

    novo teste

---
+++ src/java/br/com/barbeariaclub/util/ConnectionBD.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.barbeariaclub.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author pedro-brito
+ */
+public class ConnectionBD {
+    private final String USER = "root";
+    private final String PASSWORD = "fsadu"; //DSOcap93118"; 
+    private final String URL = "jdbc:mysql://localhost/BarbeariaClub_BD"; //jdbc:mysql://node159375-envpb.jelasticlw.com.br/barbearia";
+            //"jdbc:mysql://localhost/BarbeariaClub_BD"; 
+    Connection conn = null;
+    
+    public Connection conectarBD(){
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ConnectionBD.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }   
+}
